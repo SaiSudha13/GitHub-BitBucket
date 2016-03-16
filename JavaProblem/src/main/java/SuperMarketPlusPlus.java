b553f059c1068570f2192b013afeb11d10eb37dd
SaiSudha13 <sai.sudha123@gmail.com>
2016-03-16 16:47:18 +0000

    few more enhancements

--- JavaProblem/src/main/java/SuperMarketPlusPlus.java
+++ JavaProblem/src/main/java/SuperMarketPlusPlus.java
@@ -2,116 +2,145 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
 
-public class SuperMarketPlusPlus {
+public class SuperMarketPlusPlus.java {
 
 	private static List<Item> items = null;
-	
+
 	private final static String AGED_BRIE = "Aged Brie";
 	private final static String SULFURAS = "Sulfuras";
 	private final static String BACKSTAGE_PASSES = "Backstage Passes" ;
 	private final static String ORGANIC_BANANAS = "Organic Bananas";
-	
+	private final static int CHANGE_FACTOR_ONE = 1;
+	private final static int CHANGE_FACTOR_TWO = 2;
+	private final static int CHANGE_FACTOR_THREE = 3;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		// Start reading the Item details from itemsDetails.properties
+		System.out.println("Welcome to our SUPER MARKET ");
+		System.out.println("====================================================================");
+		System.out.println("We have started reading the Item details....");
 		readItemsDetails();
+		// Assumption : Code operation of daily schedule is already in place. 
+		// Below for loop is only for testing for 10 night cycles and can be commented out if required.
+		for(int i=0;i < 10; i++)
+			updateQuality();
+	}
+
+
+
+
+	/**
+	 * updateQuality this method does the major task of updating the Item Details
+	 * as per the requirement specifications:
+	 * 
+	 * Assumptions :
+	 * 1. It is assumed that increment of an item's Quality would max reach 50 even though there is 
+	 * 				a scope for it to be incremented more than 50, except for Sulfuras.
+	 * 2. It is assumed that decrement of an item's Quality would minimum reach 0 even though there is 
+	 * 				a scope for it to be decremented less than 0.
+	 * 
+	 */
+	public static void updateQuality()
+	{
+		String tempName = null;
+		int tempSellIn = 0;
+		int index=0;
+		System.out.println("Started Modifying the Item Details:");
+		System.out.println("====================================================================");
+
+		for(Item itemIterator : items) {
+			System.out.println("Item Name: " + itemIterator.getName());
+			System.out.println("Before Modification: SellIn="
+					+	itemIterator.getSellIn() + " Quality="
+					+	itemIterator.getQuality() );
+			tempName = itemIterator.getName();
+			tempSellIn = itemIterator.getSellIn();
+			if(AGED_BRIE.equalsIgnoreCase(tempName))
+			{
+				//Updating Quality of AGED BRIE
+				increaseQuality(CHANGE_FACTOR_ONE, itemIterator);
+			}
+			else if(BACKSTAGE_PASSES.equalsIgnoreCase(tempName))
+			{
+				//Updating Quality of BACKSTAGE PASSES Based on Special behaviour
+
+				if ( tempSellIn > 10)
+				{
+					increaseQuality(CHANGE_FACTOR_ONE, itemIterator);
+
+				}
+				else if ( tempSellIn > 5 && tempSellIn <= 10)
+				{
+					increaseQuality(CHANGE_FACTOR_TWO, itemIterator);
+				}
+				else if (tempSellIn > 0 && tempSellIn <= 5)
+				{
+					increaseQuality(CHANGE_FACTOR_THREE, itemIterator);
+				}
+				else if (tempSellIn <= 0){
+					itemIterator.setQuality(0);
+				}
+
+
+			}
+			else if (ORGANIC_BANANAS.equalsIgnoreCase(tempName))
+			{
+				//Updating Quality of ORGANIC_BANANAS Based on Special behaviour
+				decreaseQuality(CHANGE_FACTOR_TWO, itemIterator);
+			}
+			else if(!SULFURAS.equalsIgnoreCase(tempName))
+			{
+				//Updating Quality of all the other Items apart from SULFURAS, 
+
+				decreaseQuality(CHANGE_FACTOR_ONE, itemIterator); 
+			}
+
+			if (!SULFURAS.equalsIgnoreCase(tempName))
+			{
+				//Updating SellIn of all the Items apart from SULFURAS
+				itemIterator.setSellIn(tempSellIn-1);
+			}
+			
+			/*
+			 * Handling the Quality after the SellIn days reaches less than 0
+			 * 
+			 */
+			if (itemIterator.getSellIn() < 0 && !AGED_BRIE.equalsIgnoreCase(tempName) 
+					&& !BACKSTAGE_PASSES.equalsIgnoreCase(tempName) && !ORGANIC_BANANAS.equalsIgnoreCase(tempName) 
+					&& !SULFURAS.equalsIgnoreCase(tempName))
+			{
+				decreaseQuality(CHANGE_FACTOR_ONE, itemIterator);   		
+			}
 		
-  //    System.out.println("Starting Supermarket Plus Plus" +items.get(0));
-	for(int i=0;i < 10; i++)
-       updateQuality();
-}
+			// Assumed  that Banana's would degrade twice the rate after the SellIn reaches less than 0
+			else if(itemIterator.getSellIn() < 0 && ORGANIC_BANANAS.equalsIgnoreCase(tempName)) 
+			{
+				decreaseQuality(CHANGE_FACTOR_TWO, itemIterator);  
+			}
+			items.set(index, itemIterator);
+			System.out.println("After Modification : SellIn="
+					+	itemIterator.getSellIn() + " Quality="
+					+	itemIterator.getQuality() );
+			System.out.println("====================================================================");
 
+			index++;
+		}
+		System.out.println("All the necessary details of the Items have been modified Successfully ");
 
+		System.out.println();
 
-    public static void updateQuality()
-    {
-    	String tempName = null;
-    	int tempSellIn = 0;
-    	int tempQuality = 0;
-    	int index=0;
-    	ItemBehaviour itemBehaviour = new ItemBehaviour();
-    //	Iterator<Item> itemIterator = items.iterator();
-    	for(Item itemIterator : items) {
-    	//while(itemIterator.hasNext()){
-    		tempName = itemIterator.getName();
-    		tempQuality = itemIterator.getQuality();
-    		tempSellIn = itemIterator.getSellIn();
-    		if(AGED_BRIE.equalsIgnoreCase(tempName))
-    		{
-    			itemIterator.setQuality(itemBehaviour.increaseQuality(itemIterator.getQuality(),1));
-    		}
-    		else if(BACKSTAGE_PASSES.equalsIgnoreCase(tempName))
-    		{
-    			if ( tempSellIn > 10)
-                {
-    				itemIterator.setQuality(itemBehaviour.increaseQuality(itemIterator.getQuality(),1));
-            
-                }
-            	else if ( tempSellIn > 5 && tempSellIn <= 10)
-                {
-            		itemIterator.setQuality(itemBehaviour.increaseQuality(itemIterator.getQuality(),2));
-}
-                else if (tempSellIn >= 0 && tempSellIn <= 5)
-                {
-                	itemIterator.setQuality(itemBehaviour.increaseQuality(itemIterator.getQuality(),3));
-                }
-                else 
-                {
-                	itemIterator.setQuality(0);
-                }
-    			
-     			}
-    		else if (ORGANIC_BANANAS.equalsIgnoreCase(tempName))
-    		{
-    			itemIterator.setQuality(itemBehaviour.decreaseQuality(itemIterator.getQuality(),2));
-    			System.out.println(tempName + "-" +itemIterator.getQuality());
-    		}
-    		else if(!SULFURAS.equalsIgnoreCase(tempName))
-    		{
-    			itemIterator.setQuality(itemBehaviour.decreaseQuality(itemIterator.getQuality(),1)); 
-       		}
-    		 
-    		if (!SULFURAS.equalsIgnoreCase(tempName))
-            {
-    			itemIterator.setSellIn(tempSellIn-1);
-            }
-    		if (itemIterator.getSellIn() < 0 && !AGED_BRIE.equalsIgnoreCase(tempName) 
-    				&& !BACKSTAGE_PASSES.equalsIgnoreCase(tempName) && !ORGANIC_BANANAS.equalsIgnoreCase(tempName) 
-    				&& !SULFURAS.equalsIgnoreCase(tempName))
-    		{
-    			itemIterator.setQuality(itemBehaviour.decreaseQuality(itemIterator.getQuality(),1));   		
-    			System.out.println("if@@" +tempName + "-" +itemIterator.getQuality());
-        		
-        		
-    		}
-    		else if(itemIterator.getSellIn() < 0 && ORGANIC_BANANAS.equalsIgnoreCase(tempName)) 
-    		{
-    			itemIterator.setQuality(itemBehaviour.decreaseQuality(itemIterator.getQuality(),2));  
-    			System.out.println(tempName + "-" +itemIterator.getQuality());
-        		
-    		}
-    		//+itemIterator.getSellIn() + " " + 	itemIterator.getQuality() );
-    		System.out.println("Before setting " + index  +"-" + itemIterator.getName() + " "
-    			+	itemIterator.getSellIn() + " "
-    			+	itemIterator.getQuality() );
-    		
-    		items.set(index, itemIterator);
-    	//	System.out.println(items.);
-    		index++;
-    		}
-    	System.out.println();
-    	
-  //  	System.out.println(itemIterator.getName() + " " +itemIterator.getSellIn() + " " + 	itemIterator.getQuality() );
-    }	
-    		
-  /**
+		//  	System.out.println(itemIterator.getName() + " " +itemIterator.getSellIn() + " " + 	itemIterator.getQuality() );
+	}
+
+	
+	/**
 	 * This method would start reading the itemsDetails.properties file.
 	 * Only the Validated Item details would be stored and Invalid Item Details would be discarded.
 	 * An intimate is given to the User to Correct the details as per the Correct format.
@@ -169,6 +198,53 @@ public class SuperMarketPlusPlus {
 	}
 
 
-  		
-    	
+	/**
+	 * itemDetailsValidated validates for the maximum and minimum allowed Quality of an Item
+	 * 
+	 * @param name
+	 * @param quality
+	 * @return
+	 */
+	private static boolean itemDetailsValidated(String name,  int quality) {
+		// TODO Auto-generated method stub
+		if (!SULFURAS.equalsIgnoreCase(name)){
+			// Quality of all the Items, except SULFURAS Should be between 0 and 50 
+			return quality <=50 &&  quality>=0 ;
+		}
+		else {
+			// Quality of SULFURAS should be between 0 and 80
+			return quality <=80 &&  quality>=0  ;
+
+		}
+	}
+
+
+
+
+
+
+	/**
+	 * @param changeFactor
+	 * @param itemBehaviour
+	 * @param itemIterator
+	 */
+	public static void decreaseQuality(int changeFactor,
+			Item itemIterator) {
+		itemIterator.setQuality(new ItemBehaviour().decreaseQuality(itemIterator.getQuality(),changeFactor));
+	}
+
+
+
+	/**
+	 * @param changeFactor
+	 * @param itemBehaviour
+	 * @param itemIterator
+	 */
+	public static void increaseQuality(int changeFactor,
+			Item itemIterator) {
+		itemIterator.setQuality(new ItemBehaviour().increaseQuality(itemIterator.getQuality(),changeFactor));
+	}	
+
+
+
 }
